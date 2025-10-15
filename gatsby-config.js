94f932f1d250522e87c7f0034d91de5dcb4a23eb
Paul Sutcliffe <paulyoyo@gmail.com>
2025-10-14 19:43:09 -0500

    Primer Commit

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: "3C Capital",
+    description: "Factoring y Oportunidades de Rentabilidad",
+  },
+  plugins: [],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Sitio web 3C Capital</h1>
+      <p>Holi</p>
+    </main>
+  );
+}
